2dca3d849c176254fc0527d52d20da7d891d733a
Reinis Ikass <reinis.ikass@gmail.com>
2023-06-30 16:21:02 +0300

    feat: implement mobile menu

--- frontend/javascript/toggle.js
+++ frontend/javascript/toggle.js
@@ -1,9 +1,11 @@
 const hamburger = document.querySelector("#hamburger");
+const nav = document.querySelector("#main-nav");
 
 hamburger.addEventListener("click", (e) => {
   // aria-expanded="true" signals that the menu is currently open
   const isOpen = hamburger.getAttribute("aria-expanded") === "true";
   hamburger.setAttribute("aria-expanded", !isOpen);
-  // Toggle 'close' class
+  // Toggle 'close' class to change the icon
   hamburger.classList.toggle("close");
+  nav.classList.toggle("visible");
 });
